c7fc8b97c7919b524a954c93388daf03889b75b3
sora <133684349+sorawaslocked@users.noreply.github.com>
2025-05-13 15:34:56 +0500

    Refactor logger setup from main project

---
+++ pkg/logger/logger.go
@@ -0,0 +1,46 @@
+package logger
+
+import (
+	"github.com/sorawaslocked/ap2final_base/pkg/logger/slogpretty"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func SetupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettyLogger()
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(
+			os.Stdout,
+			&slog.HandlerOptions{Level: slog.LevelDebug},
+		))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(
+			os.Stdout,
+			&slog.HandlerOptions{Level: slog.LevelInfo},
+		))
+	}
+
+	return log
+}
+
+func setupPrettyLogger() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
+}
